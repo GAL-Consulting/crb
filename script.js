a539fb19787244b47ba56cb62e2d9a200d08879b
GAL-Consulting <consultasgalrna@gmail.com>
2025-10-13 01:20:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -205,7 +205,7 @@ function llenarSelect(id, opciones) {
       gapi.client.drive.files.list({
       //  q: `'${carpetaId}' in parents and mimeType='application/pdf' and trashed=false`,
       //  q: `'${carpetaId}' in parents and trashed=false`,
-        q: `'${carpetaId}' in parents and mimeType!='application/vnd.google-apps.folder' and trashed=false`
+        q: `'${carpetaId}' in parents and mimeType!='application/vnd.google-apps.folder' and trashed=false`,
 
        // fields: "files(id, name, webViewLink)",
         fields: "files(id, name, mimeType, webViewLink, webContentLink)",
